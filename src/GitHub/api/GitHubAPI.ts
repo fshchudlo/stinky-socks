e7a5797d3fbef2df461fe40839af8dfb58e4dc73
fshchudlo <fshchudlo@gmail.com>
2024-10-24 20:33:17 +0300

    Nifty rename

--- src/GitHub/api/GitHubAPI.ts
+++ src/GitHub/api/GitHubAPI.ts
@@ -4,11 +4,11 @@ import { GitHubFileDiffModel, GitHubPullRequestActivityModel, GitHubPullRequestM
 export class GitHubAPI {
     private readonly token: string;
     private readonly baseUrl = "https://api.github.com";
-    private readonly addressRateLimits: boolean;
+    private readonly pauseOnRateLimitThreshold: boolean;
 
     constructor(token: string, addressRateLimits: boolean = true) {
         this.token = token;
-        this.addressRateLimits = addressRateLimits;
+        this.pauseOnRateLimitThreshold = addressRateLimits;
     }
 
     private async get(url: string, params: any = undefined): Promise<any> {
@@ -30,7 +30,7 @@ export class GitHubAPI {
     }
 
     private async checkRateLimits(response: AxiosResponse<any>) {
-        if (this.addressRateLimits && response.headers["x-ratelimit-remaining"] === "1") {
+        if (this.pauseOnRateLimitThreshold && response.headers["x-ratelimit-remaining"] === "1") {
             const resetTime = parseInt(response.headers["x-ratelimit-reset"], 10) * 1000;
             const currentTime = Date.now();
             const waitTime = resetTime - currentTime;
@@ -86,7 +86,7 @@ export class GitHubAPI {
         const url = `${this.baseUrl}/repos/${owner}/${repo}/issues/${pullRequestId}/timeline`;
         const result = await this.getFullList(url);
         return result.map(a => {
-            if (!["auto_rebase_enabled", "base_ref_force_pushed", "connected", "auto_merge_disabled", "auto_squash_enabled", "convert_to_draft", "unsubscribed", "locked", "removed_from_project", "moved_columns_in_project", "added_to_project", "review_dismissed", "comment_deleted", "base_ref_changed", "review_request_removed", "unassigned", "reopened", "demilestoned", "line-commented", "head_ref_restored", "automatic_base_change_succeeded", "ready_for_review", "reviewed", "review_requested", "cross-referenced", "referenced", "commented", "committed", "merged", "closed", "unlabeled", "milestoned", "renamed", "labeled", "head_ref_deleted", "head_ref_force_pushed", "mentioned", "subscribed", "assigned"].includes(a.event)) {
+            if (!["disconnected", "auto_rebase_enabled", "base_ref_force_pushed", "connected", "auto_merge_disabled", "auto_squash_enabled", "convert_to_draft", "unsubscribed", "locked", "removed_from_project", "moved_columns_in_project", "added_to_project", "review_dismissed", "comment_deleted", "base_ref_changed", "review_request_removed", "unassigned", "reopened", "demilestoned", "line-commented", "head_ref_restored", "automatic_base_change_succeeded", "ready_for_review", "reviewed", "review_requested", "cross-referenced", "referenced", "commented", "committed", "merged", "closed", "unlabeled", "milestoned", "renamed", "labeled", "head_ref_deleted", "head_ref_force_pushed", "mentioned", "subscribed", "assigned"].includes(a.event)) {
                 throw new Error(`Unknown event type: ${a.event}`);
             }
             return a;
