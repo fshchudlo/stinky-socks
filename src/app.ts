8483f4c1102de85aafb5396fa2c3f06d39989a4d
fshchudlo <fshchudlo@gmail.com>
2024-10-25 21:11:51 +0300

    Nifty cleanups

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,7 @@
 import "reflect-metadata";
 
 import { MetricsDB } from "./MetricsDB/MetricsDB";
-import { BitbucketAPI } from "./Bitbucket/api/BitbucketAPI";
-import { BitbucketPullRequestsImporter } from "./Bitbucket/BitbucketPullRequestsImporter";
-import { appImportConfig, TeamImportSettings } from "./app.importConfig";
-import { GitHubPullRequestsImporter } from "./GitHub/GitHubPullRequestsImporter";
-import { GitHubAPI } from "./GitHub/api/GitHubAPI";
-import { ContributorFactory } from "./MetricsDB/ContributorFactory";
+import importTeamProjects from "./importTeamProjects";
 
 let isImportRunning = false;
 
@@ -23,54 +18,11 @@ async function runDataImports() {
     }
 }
 
-async function importTeamProjects() {
-    const timelogLabel = `🎉 Teams data import completed!`;
-    console.time(timelogLabel);
-    console.group("🚀 Starting Pull Requests import...");
-
-    for (const team of appImportConfig.teams) {
-        console.log(`🔁 Importing pull requests for '${team.teamName}' team`);
-
-        await ContributorFactory.preloadCacheByTeam(team.teamName);
-        await importBitbucketProjects(team);
-        await importGitHubPullRequests(team);
-    }
-
-    console.groupEnd();
-    console.timeEnd(timelogLabel);
-}
-
-async function importBitbucketProjects(team: TeamImportSettings) {
-    const timelogLabel = `🎉 Bitbucket pull requests import completed!`;
-    console.time(timelogLabel);
-
-    for (const bitbucketProject of team.bitbucketProjects || []) {
-        console.group(`🔁 Importing pull requests for '${bitbucketProject.projectKey}' project`);
-
-        const bitbucketAPI = new BitbucketAPI(bitbucketProject.auth.apiUrl, bitbucketProject.auth.apiToken);
-        await new BitbucketPullRequestsImporter(bitbucketAPI, team.teamName, bitbucketProject).importPullRequests();
-
-        console.log(`🔁 Import of pull requests for '${bitbucketProject.projectKey}' project completed`);
-        console.groupEnd();
-    }
-    console.timeEnd(timelogLabel);
-}
-
-async function importGitHubPullRequests(team: TeamImportSettings) {
-    const timelogLabel = `🎉 GitHub pull requests import completed!`;
-    console.time(timelogLabel);
-
-    for (const gitHubProject of team.gitHubProjects || []) {
-        console.group(`🔁 Importing pull requests for '${gitHubProject.owner}' project`);
-
-        const gitHubAPI = new GitHubAPI(gitHubProject.auth.apiToken, true);
-        await new GitHubPullRequestsImporter(gitHubAPI, team.teamName, gitHubProject).importPullRequests();
-
-        console.log(`🔁 Import of pull requests for '${gitHubProject.owner}' project completed`);
-        console.groupEnd();
-    }
-    console.timeEnd(timelogLabel);
-}
+const originalLog = console.log;
+console.log = (...args) => {
+    const timestamp = new Date().toLocaleTimeString();
+    originalLog(`[${timestamp}]`, ...args);
+};
 
 MetricsDB.initialize().then(() => {
     runDataImports().catch(error => console.error(error));
---
+++ src/importTeamProjects.ts
@@ -0,0 +1,55 @@
+import { appImportConfig, TeamImportSettings } from "./app.importConfig";
+import { ContributorFactory } from "./MetricsDB/ContributorFactory";
+import { BitbucketAPI } from "./Bitbucket/api/BitbucketAPI";
+import { BitbucketPullRequestsImporter } from "./Bitbucket/BitbucketPullRequestsImporter";
+import { GitHubAPI } from "./GitHub/api/GitHubAPI";
+import { GitHubPullRequestsImporter } from "./GitHub/GitHubPullRequestsImporter";
+
+export default async function importTeamProjects() {
+    const timelogLabel = `🎉 Teams data import completed!`;
+    console.time(timelogLabel);
+    console.group("🚀 Starting Pull Requests import...");
+
+    for (const team of appImportConfig.teams) {
+        console.log(`🔁 Importing pull requests for '${team.teamName}' team`);
+
+        await ContributorFactory.preloadCacheByTeam(team.teamName);
+        await importBitbucketProjects(team);
+        await importGitHubPullRequests(team);
+    }
+
+    console.groupEnd();
+    console.timeEnd(timelogLabel);
+}
+
+async function importBitbucketProjects(team: TeamImportSettings) {
+    const timelogLabel = `🎉 Bitbucket pull requests import completed!`;
+    console.time(timelogLabel);
+
+    for (const bitbucketProject of team.bitbucketProjects || []) {
+        console.group(`🔁 Importing pull requests for '${bitbucketProject.projectKey}' project`);
+
+        const bitbucketAPI = new BitbucketAPI(bitbucketProject.auth.apiUrl, bitbucketProject.auth.apiToken);
+        await new BitbucketPullRequestsImporter(bitbucketAPI, team.teamName, bitbucketProject).importPullRequests();
+
+        console.log(`🔁 Import of pull requests for '${bitbucketProject.projectKey}' project completed`);
+        console.groupEnd();
+    }
+    console.timeEnd(timelogLabel);
+}
+
+async function importGitHubPullRequests(team: TeamImportSettings) {
+    const timelogLabel = `🎉 GitHub pull requests import completed!`;
+    console.time(timelogLabel);
+
+    for (const gitHubProject of team.gitHubProjects || []) {
+        console.group(`🔁 Importing pull requests for '${gitHubProject.owner}' project`);
+
+        const gitHubAPI = new GitHubAPI(gitHubProject.auth.apiToken, true);
+        await new GitHubPullRequestsImporter(gitHubAPI, team.teamName, gitHubProject).importPullRequests();
+
+        console.log(`🔁 Import of pull requests for '${gitHubProject.owner}' project completed`);
+        console.groupEnd();
+    }
+    console.timeEnd(timelogLabel);
+}
