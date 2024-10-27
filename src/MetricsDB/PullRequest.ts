b78988c60c25959f6d71ea8ac265ba8ab1ca17eb
fshchudlo <fshchudlo@gmail.com>
2024-10-27 09:57:30 +0200

    Added author role to the dashboard filters

--- src/MetricsDB/PullRequest.ts
+++ src/MetricsDB/PullRequest.ts
@@ -53,7 +53,7 @@ export abstract class PullRequest {
     protected testsWereTouched: boolean;
 
     @Column({ type: "varchar" })
-    protected authorRole: "COLLABORATOR" | "CONTRIBUTOR" | "FIRST_TIMER" | "FIRST_TIME_CONTRIBUTOR" | "MEMBER" | "OWNER" | "MANNEQUIN" | "NONE";
+    protected authorRole: PullRequestAuthorRole;
 
     @OneToMany(() => PullRequestParticipant, (participant) => participant.pullRequest, {
         cascade: true
@@ -64,3 +64,21 @@ export abstract class PullRequest {
     @JoinColumn()
     author: Contributor;
 }
+
+export type PullRequestAuthorRole =
+// The author is the owner of the repository.
+    "OWNER"
+    // The author is a member of the organization that owns the repository.
+    | "MEMBER"
+    // The author has write access to the repository.
+    | "COLLABORATOR"
+    // The author created his first contribution to any repository in the organization.
+    | "FIRST_TIMER"
+    // The author contributed to this particular repository for the first time.
+    | "FIRST_TIME_CONTRIBUTOR"
+    // The author has previously committed to the repository but is not necessarily a member or collaborator.
+    | "CONTRIBUTOR"
+    // The author is a placeholder for a previously deleted user
+    | "MANNEQUIN"
+    // The author has no affiliation with the repository.
+    | "NONE";
