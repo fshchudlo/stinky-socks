b31c3bd8fb94360096fcc844302f0101bb14b0b7
fshchudlo <fshchudlo@gmail.com>
2024-10-30 01:51:01 +0200

    Fixed bug with an undefined user in the line comments

--- src/GitHub/entities/helpers/getActivitiesOf.ts
+++ src/GitHub/entities/helpers/getActivitiesOf.ts
@@ -4,7 +4,7 @@ import { ActivityTraits } from "./ActivityTraits";
 export default function getActivitiesOf(activities: GitHubPullRequestActivityModel[], userName: string) {
     return activities.filter(a => {
         if (ActivityTraits.isLineCommentedEvent(a)) {
-            return a.comments.map(c => c.user.login).includes(userName);
+            return a.comments.map(c => c.user?.login).includes(userName);
         }
 
         const typedA = a as any;
