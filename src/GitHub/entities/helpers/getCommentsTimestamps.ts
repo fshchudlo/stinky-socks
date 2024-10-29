924551467b2b98ebf98a4d1a9473ee847a84cba4
fshchudlo <fshchudlo@gmail.com>
2024-10-29 23:04:34 +0200

    Added handling of null users

--- src/GitHub/entities/helpers/getCommentsTimestamps.ts
+++ src/GitHub/entities/helpers/getCommentsTimestamps.ts
@@ -4,12 +4,12 @@ import { ActivityTraits } from "./ActivityTraits";
 export default function getCommentsTimestamps(activities: GitHubPullRequestActivityModel[], botUsers: string[] = []) {
     const comments = activities
         .filter(ActivityTraits.isCommentedEvent)
-        .filter(a => !botUsers.includes(a.user.login))
+        .filter(a => !(a.user && botUsers.includes(a.user.login)))
         .map(c => new Date(c.created_at).getTime());
 
     const lineComments = activities.filter(ActivityTraits.isLineCommentedEvent)
         .flatMap(a => a.comments)
-        .filter(a => !botUsers.includes(a.user.login))
+        .filter(a => !(a.user && botUsers.includes(a.user.login)))
         .map(c => new Date(c.created_at).getTime());
 
     const reviewComments = activities.filter(ActivityTraits.isReviewedEvent)
