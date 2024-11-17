f799b5b167375c9702825c24ae453043e9cebbdb
fshchudlo <fshchudlo@gmail.com>
2024-11-17 22:37:06 +0200

    Added the first basic test

--- src/GitHub/entities/tests/GitHubPullRequest.tests.ts
+++ src/GitHub/entities/tests/GitHubPullRequest.tests.ts
@@ -2,26 +2,29 @@ import { GitHubPullRequest } from "../GitHubPullRequest";
 import { ActorFactory } from "../../../MetricsDB/ActorFactory";
 import { TestPRsBuilder } from "./TestPRsBuilder";
 
+let fetchCallsCounter = 0;
+jest.spyOn(ActorFactory, "fetch").mockImplementation(async ({ teamName, login, isBotUser }: {
+    teamName: string,
+    login: string,
+    isBotUser: boolean
+}) => {
+    return Promise.resolve({
+        id: ++fetchCallsCounter,
+        login: login,
+        teamName: teamName,
+        nickname: `${login} nickname`,
+        isBotUser: isBotUser,
+        isFormerParticipant: false,
+        duplicates: [],
+        participations: [],
+        pullRequests: [],
+        teamRole: null
+    });
+});
+
 describe("GitHubPullRequest", () => {
     it("Should map basic fields", async () => {
-        jest.spyOn(ActorFactory, "fetch").mockImplementation(async ({ teamName, login, isBotUser }: {
-            teamName: string,
-            login: string,
-            isBotUser: boolean
-        }) => {
-            return Promise.resolve({
-                id: 1,
-                login: login,
-                teamName: teamName,
-                nickname: `${login} nickname`,
-                isBotUser: isBotUser,
-                isFormerParticipant: false,
-                duplicates: [],
-                participations: [],
-                pullRequests:[],
-                teamRole: null
-            });
-        });
+
         const model = new TestPRsBuilder().pullRequest();
 
         const pullRequestEntity = await new GitHubPullRequest().init(model);
